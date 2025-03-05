01d9d6ce7e2b992c6b600057a5151727ef895d10
AndreaRispoli97 <Andrea9715@outlook.it>
2025-03-05 16:56:58 +0100

    add: milestone1

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 console.log('Andrea');
+// Descrizione:
+//Scrivere un programma che chieda all’utente:
+//- Il numero di chilometri da percorrere
+//    - Età del passeggero
+//Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+//- il prezzo del biglietto è definito in base ai km(0.21 € al km)
+//    - va applicato uno sconto del 20 % per i minorenni
+//        - va applicato uno sconto del 40 % per gli over 65.
+//MILESTONE 1:
+//Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone(non stilizzati), realizziamo le specifiche scritte sopra.La risposta finale(o output) sarà anch’essa da scrivere in console.
+
+
+const inputKm = document.getElementById("input-km");
+const inputAge = document.getElementById("input-age");
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener('click', function () {
+    const km = inputKm.value;
+    console.log('Quanti chilometri devi percorrere?');
+    const age = inputAge.value;
+    console.log('Quanti anni hai?');
+    const price = 0.21;
+    let finalPrice = price * km;
+    if (age < 18) {
+        finalPrice = finalPrice - (finalPrice * 20 / 100);
+    } else if (age > 65) {
+        finalPrice = finalPrice - (finalPrice * 40 / 100);
+    }
+    console.log(`Il prezzo finale del tuo biglietto è ${finalPrice.toFixed(2)}€`);
+})
+
